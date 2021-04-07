823c493f17d8062bf345aac76d7bee1874c1a431
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-07 22:09:02 +0600

    successfully print logo and header for pdf

--- main.go
+++ main.go
@@ -4,19 +4,58 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/johnfercher/maroto/pkg/color"
 	"github.com/johnfercher/maroto/pkg/consts"
 	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
 )
 
 func main() {
 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 	m.SetPageMargins(20, 10, 20)
 
-	err := m.OutputFileAndClose("raihan/raihan.pdf")
+	buildHeading(m)
+
+	err := m.OutputFileAndClose("pdf/raihan.pdf")
 	if err != nil {
 		fmt.Println("Could not save PDF:", err)
 		os.Exit(1)
 	}
 
 	fmt.Println("PDF saved successfully")
+}
+
+func buildHeading(m pdf.Maroto) {
+	m.RegisterHeader(func() {
+		m.Row(50, func() {
+			m.Col(12, func() {
+				err := m.FileImage("images/image.jpg", props.Rect{
+					Center:  true,
+					Percent: 75,
+				})
+				if err != nil {
+					fmt.Println("iamge file was not loded: ", err)
+				}
+			})
+		})
+	})
+
+	m.Row(10, func() {
+		m.Col(12, func() {
+			m.Text("This Pdf for google company", props.Text{
+				Top:   3,
+				Style: consts.Bold,
+				Align: consts.Center,
+				Color: getDarkPurpleColor(),
+			})
+		})
+	})
+}
+
+func getDarkPurpleColor() color.Color {
+	return color.Color{
+		Red:   88,
+		Green: 80,
+		Blue:  99,
+	}
 }
